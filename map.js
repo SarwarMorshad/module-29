e6f4661fbf394a1e82ca8d6c17521b6cc0193797
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-09-04 09:33:28 +0200

    For Each

--- map.js
+++ map.js
@@ -3,3 +3,10 @@
 const numbers = [1, 2, 3, 4, 5];
 const doubled = numbers.map((x) => x * 2);
 console.log(doubled);
+
+// First Index Length
+const friends = ["Alice", "Bob", "Charlie"];
+const nameLength = friends.map((friend) => friend.length);
+const firstNameLength = nameLength[0];
+console.log(nameLength);
+console.log(firstNameLength);
