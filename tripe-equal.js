e141fe5cb0b04ea4f9c02e52a5b5a2051f0586e9
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-09-05 12:07:53 +0200

    == vs ===

---
+++ tripe-equal.js
@@ -0,0 +1,5 @@
+//  ! Difference between == and ===
+//  == check only value, === check value and type
+
+console.log(2 == "2"); // true
+console.log(2 === "2"); // false
--- truthy.js
+++ truthy.js
@@ -2,6 +2,12 @@ let data;
 data = 0;
 // data = ""; Empty string is falsy
 data = " "; // Space is truthy
+data = false; // false is falsy
+data = true; // true is truthy
+data = null; // null is falsy
+data = {}; // Empty object is truthy
+data = []; // Empty array is truthy
+
 if (data) {
   console.log("Truthy");
 } else {
